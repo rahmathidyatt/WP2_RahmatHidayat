a176752e9556c0a60bffc524c24678bac7d1e8ac
rahmathidyatt <amathmrs02@gmail.com>
2021-10-13 16:35:27 +0700

    Upload file

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan    : Rahmat</li> 
+        <li>Nama Belakang : Hidayat</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jakarta Pusat</li> 
+    </ul> 
+    
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Wawondengi</li> 
+    </ul> 
+    
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Futsal</li> 
+        <li>Bulu Tangkis</li> 
+    </ul> 
+</section>
